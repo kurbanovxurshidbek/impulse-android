2ac58cfb5c3c2aba364a9de72b8506a523824978
Mirzohid Dilshodov <md20021203@gmail.com>
2022-05-16 14:35:00 +0500

    test

--- app/src/main/java/com/impulse/impulse/activity/MainActivity.kt
+++ app/src/main/java/com/impulse/impulse/activity/MainActivity.kt
@@ -2,11 +2,13 @@ package com.impulse.impulse.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.impulse.impulse.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Toast.makeText(this, "Created Main", Toast.LENGTH_SHORT).show()
     }
 }
