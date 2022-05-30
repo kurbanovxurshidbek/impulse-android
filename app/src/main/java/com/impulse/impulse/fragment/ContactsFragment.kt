232c62000a1176dcba9806118f024809e61238f7
Mirzohid Dilshodov <md20021203@gmail.com>
2022-05-30 20:43:49 +0500

    room db removed

--- app/src/main/java/com/impulse/impulse/fragment/ContactsFragment.kt
+++ app/src/main/java/com/impulse/impulse/fragment/ContactsFragment.kt
@@ -5,12 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.impulse.impulse.R
-import com.impulse.impulse.adapter.ContactsItemAdapter
-import com.impulse.impulse.database.AppDatabase
 import com.impulse.impulse.databinding.FragmentContactsBinding
-import com.impulse.impulse.model.Contact
-import com.impulse.impulse.model.ContactNestedItem
 import com.impulse.impulse.utils.SpacesItemDecoration
 
 class ContactsFragment : BaseFragment() {
@@ -20,7 +15,6 @@ class ContactsFragment : BaseFragment() {
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
-    private lateinit var appDatabase: AppDatabase
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -28,7 +22,6 @@ class ContactsFragment : BaseFragment() {
     ): View {
         _binding = FragmentContactsBinding.inflate(inflater, container, false)
         val view = binding.root
-        appDatabase = AppDatabase.getInstance(requireContext())
         initViews()
         return view
     }
@@ -45,35 +38,7 @@ class ContactsFragment : BaseFragment() {
                 LinearLayoutManager(context)
             val decoration = SpacesItemDecoration(20)
             recyclerView.addItemDecoration(decoration)
-            recyclerView.adapter = ContactsItemAdapter(requireContext(), getAllContacts())
 
         }
-    }
-
-    private fun getAllContacts(): ArrayList<Contact> {
-        val items = ArrayList<Contact>()
-        val nestedItems = ArrayList<ContactNestedItem>()
-        nestedItems.add(ContactNestedItem("+998901203022", "Help"))
-        items.add(
-            Contact(
-                "https://images.unsplash.com/photo-1653587106660-4908e9a7bae7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60",
-                "John", "Son", false, nestedItems
-            )
-        )
-
-        items.add(
-            Contact(
-                "https://images.unsplash.com/photo-1653587106660-4908e9a7bae7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60",
-                "John", "Son", false, nestedItems
-            )
-        )
-
-        items.add(
-            Contact(
-                "https://images.unsplash.com/photo-1653587106660-4908e9a7bae7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60",
-                "John", "Son", false, nestedItems
-            )
-        )
-        return items
     }
 }
