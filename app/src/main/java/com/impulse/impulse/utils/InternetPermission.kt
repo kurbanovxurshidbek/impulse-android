4de6f02d5004e8170c3ba30ea267ddb19ba49b67
IlhombekUbaydullayev <ubaydullaevilhombek682@gmail.com>
2022-06-11 15:46:50 +0500

    initial

---
+++ app/src/main/java/com/impulse/impulse/utils/InternetPermission.kt
@@ -0,0 +1,16 @@
+package com.impulse.impulse.utils
+
+import android.content.Context
+import android.content.Context.CONNECTIVITY_SERVICE
+import android.net.ConnectivityManager
+
+class InternetPermission {
+    companion object {
+        fun isInternetAvailable(context: Context): Boolean {
+            val manager = context.getSystemService(CONNECTIVITY_SERVICE) as ConnectivityManager
+            val infoMobile = manager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE)
+            val infoWifi = manager.getNetworkInfo(ConnectivityManager.TYPE_WIFI)
+            return infoMobile!!.isConnected || infoWifi!!.isConnected
+        }
+    }
+}
